1c7d1f240f72a369951853f0c0929c64ad3b7c23
Rafael-uro <94479855+Rafael-uro@users.noreply.github.com>
2023-05-10 19:59:08 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyA0HjVe9o8xrqsS_Xuwpqaso-ZeeagfIxk",
+  authDomain: "c85-aabe1.firebaseapp.com",
+  databaseURL: "https://c85-aabe1-default-rtdb.firebaseio.com",
+  projectId: "c85-aabe1",
+  storageBucket: "c85-aabe1.appspot.com",
+  messagingSenderId: "606318264170",
+  appId: "1:606318264170:web:0cc553de98278a9f3cef13"
+};
+
